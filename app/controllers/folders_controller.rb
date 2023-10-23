f6fb26625821967d633c24be106834a818eb6dfe
taichikitano <taichikitano@gmail.com>
2023-10-23 23:45:59 +0900

    フォルダ保存機能実装

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -4,4 +4,19 @@ class FoldersController < ApplicationController
     @folder = Folder.new
   end
 
+  def create
+    @folder = Folder.new(folder_params)
+    if @folder.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def folder_params
+    params.require(:folder).permit(:name, user_ids: [])
+  end
+
 end
--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,4 +1,6 @@
 class Folder < ApplicationRecord
   has_many :folder_users
   has_many :users, through: :folder_users
+
+  validates :name, presence: true
 end
