8e6c8197c72eea098c1702578a9c04b0bc287f96
taichikitano <taichikitano@gmail.com>
2023-10-24 04:13:02 +0900

    ノート編集機能実装

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,11 @@
 class NotesController < ApplicationController
 
   def index
+    @folder = Folder.find(params[:folder_id])
+    @notes = @folder.notes.includes(:user)
+  end
+
+  def new
     @note = Note.new
     @folder = Folder.find(params[:folder_id])
     @notes = @folder.notes.includes(:user)
@@ -13,7 +18,24 @@ class NotesController < ApplicationController
       redirect_to folder_notes_path(@folder)
     else
       @notes = @folder.notes.includes(:user)
-      render :index, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+    @folder = Folder.find(params[:folder_id])
+    @notes = @folder.notes.includes(:user)
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    @folder = Folder.find(params[:folder_id])
+    if @note.update(note_params)
+      redirect_to edit_folder_note_path(@folder, @note), notice: 'ノートが更新されました'
+    else
+      @notes = @folder.notes.includes(:user)
+      render :edit, status: :unprocessable_entity
     end
   end
 
